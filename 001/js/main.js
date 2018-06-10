f75d451cf51a3ecb54aca266c4e495bb6d7c5231
小儀 <pinfudesign@gmail.com>
2018-06-10 19:09:23 +0800

    新增刪除事件、完成清單事件

--- 001/js/main.js
+++ 001/js/main.js
@@ -14,35 +14,6 @@ $(document).ready(function () {
     return false;
 });
 
-// 按鈕ckeck事件
-$('#addNew').on('click', function () {
-    var contents = $('#event').val();
-    var darggerSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><circle cy = "7" cx = "9.5" r = "1" fill = "#999" ></circle><circle cy="7" cx="14" r="1" fill="#999"></circle><circle cy="12.5" cx="9.5" r="1" fill="#999"></circle><circle cy="12.5" cx="14" r="1" fill="#999"></circle><circle cy="18" cx="9.5" r="1" fill="#999"></circle><circle cy="18" cx="14" r="1" fill="#999"></circle></svg>';
-
-    $('#listMain').prepend(`
-        <div class="card border-0 mb-3">
-            <div class="card-body pl-1 pr-3 d-flex align-items-center">
-                <div class="dragger mr-1">${darggerSVG}</div>
-                <button type="button" id="complete" class="btn btn-outline-check">
-                <i class="fas fa-check"></i>
-                </button>
-                <div class="listContent ml-2">${contents}</div>
-                <div class="actions ml-auto">
-                <a href="#" class="btn btn-edit">
-                    <i class="fas fa-pencil-alt"></i>
-                </a>
-                <a href="#" class="btn btn-delete">
-                    <i class="fas fa-trash-alt"></i>
-                </a>
-                </div>
-            </div>
-        </div>
-    `);
-
-    $('#event').val('');
-    return false;
-});
-
 // Sortable plugin 拖曳事件
 $(document).ready(function () {
     var el = document.getElementById("listMain");
@@ -58,4 +29,79 @@ $(document).ready(function () {
         chosenClass: "sortable-chosen",  // 選定時，給予物件的類別
         forceFallback: false  // 忽略HTML5 DnD
     });
+    return false;
 });
+
+// 新增事件
+$('#addNew').on('click', function addEvent() {
+    // 新增事件
+    var contents = $('#event').val();
+    var darggerSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><circle cy = "7" cx = "9.5" r = "1" fill = "#999" ></circle><circle cy="7" cx="14" r="1" fill="#999"></circle><circle cy="12.5" cx="9.5" r="1" fill="#999"></circle><circle cy="12.5" cx="14" r="1" fill="#999"></circle><circle cy="18" cx="9.5" r="1" fill="#999"></circle><circle cy="18" cx="14" r="1" fill="#999"></circle></svg>';
+
+    $('#listMain').prepend(`
+        <div class="card border-0 mb-3">
+            <div class="card-body pl-1 pr-3 d-flex align-items-center">
+                <div class="dragger mr-1">${darggerSVG}</div>
+                <a id="complete" class="btn btn-outline-check">
+                    <i class="fas fa-check"></i>
+                </a>
+                <div class="listContent ml-2"><p class="mb-0">${contents}</p></div>
+                <div class="actions ml-auto">
+                <a href="#" class="btn btn-edit">
+                    <i class="fas fa-pencil-alt"></i>
+                </a>
+                <a href="#" class="btn btn-delete">
+                    <i class="fas fa-trash-alt"></i>
+                </a>
+                </div>
+            </div>
+        </div>
+    `);
+
+
+    $('#event').val('');
+
+
+    // 刪除事件
+    $('.btn-delete').on('click', function () {
+        $(this).closest('.card').remove();
+    });
+
+    return false;
+});
+
+
+
+
+
+$(document).ready(function () {
+    // 刪除預設事件
+    $('.btn-delete').on('click', function () {
+        $(this).closest('.card').remove();
+    });
+
+    // 點下完成按鈕事件
+    $('.listArea #listMain').on('click', '#complete', function () {
+
+        // 找到點擊的按鈕
+        let button = $(this);
+
+        // 找到相對應的文字
+        let index_click = $('a#complete').index(this);
+        let linethrough_text = $('.card-body .listContent').eq(index_click).find('p');
+
+        $(button).toggleClass('checked');
+        $(linethrough_text).toggleClass('line-through');
+
+        // 隱藏編輯按鈕
+        // $('a.btn-edit').toggleClass('hidden');
+    });
+
+    $('body').on('click', '.fa-pencil', function () {
+        var current = $(this).parent().parent().find('span').text();
+        $(this).parent().parent().find('input[type=text]').val(current).show().select();
+    });
+    return false;
+});
+
+
