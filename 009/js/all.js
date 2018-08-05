647419304a10ea3a736f79150500a5baeecad9b2
小儀 <pinfudesign@gmail.com>
2018-08-06 02:03:08 +0800

    wee9 ver.1 - 無RWD 待補：

---
+++ 009/js/all.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.skillMain').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
